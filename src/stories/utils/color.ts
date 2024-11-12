59b7f9193a3273c452561db59fdff0b705ee4276
lin-mt <lin-mt@outlook.com>
2024-11-13 07:52:57 +0800

    update code

--- src/stories/utils/color.ts
+++ src/stories/utils/color.ts
@@ -2,6 +2,6 @@ import React from "react";
 import type {GlobalToken} from "antd/es/theme/interface";
 
 
-export function getActiveBgColor(isActive: boolean, token: GlobalToken): React.CSSProperties | undefined {
-  return isActive ? {backgroundColor: token.controlItemBgActive} : undefined;
+export function getActiveBgColor(isActive: boolean, token: GlobalToken): React.CSSProperties {
+  return isActive ? {backgroundColor: token.controlItemBgActive} : {};
 }
